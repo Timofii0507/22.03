6e9a28fc03fa8199889fa30cf2ad0b6993df3ed2
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-22 19:47:50 +0200

    П'яте завдання  з  практичної  роботи

---
+++ Program5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._03
+{
+    class Program
+    {
+        delegate int MaxFinder(int[] numbers);
+
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            int[] array = { 1, 3, 5, 7, 8 };
+
+            MaxFinder findMax = (numbers) =>
+            {
+                int max = numbers[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] > max)
+                    {
+                        max = numbers[i];
+                    }
+                }
+                return max;
+            };
+
+            Console.WriteLine("Максимальне значення в масиві: " + findMax(array));
+        }
+    }
+}
