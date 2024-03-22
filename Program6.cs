44534f39a94d97f0ebee006257771076003e54f9
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-22 20:12:20 +0200

    Шосте завдання  з  практичної роботи

---
+++ Program6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _22._03
+{
+    class Program6
+    {
+        public delegate int ComparisonDelegate(int a, int b);
+
+        static int Min(int[] array, ComparisonDelegate comparer)
+        {
+            int minIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (comparer(array[minIndex], array[i]) > 0)
+                {
+                    minIndex = i;
+                }
+            }
+            return array[minIndex];
+        }
+
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            int[] numbers = { 5, 8, 3, 1, 2, 9, 4, 7, 6 };
+
+            int min = Min(numbers, (a, b) => a - b);
+
+            Console.WriteLine($"Мінімальне значення: {min}");
+
+            min = Min(numbers, (a, b) => b - a);
+
+            Console.WriteLine($"Максимальне значення: {min}");
+        }
+    }
+}
