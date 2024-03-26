457fe241532e6bd316e35671b12c0fccbdabe816
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-26 19:40:34 +0200

    Четверте завдання  з практичної роботи

--- Program4.cs
+++ Program4.cs
@@ -6,32 +6,20 @@ using System.Threading.Tasks;
 
 namespace _22._03
 {
-    public delegate bool IsProgrammerDayDelegate(DateTime date);
-
     class Program4
     {
-        public static event EventHandler DateChecked;
-
-        static void Main(string[] args)
+        static void Main()
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.CursorVisible = false;
-            IsProgrammerDayDelegate isProgrammerDay = (date) => date.DayOfYear == 256;
+            Func<DateTime, bool> isProgrammersDay = date => date.DayOfYear == 256;
+            DateTime testDate = new DateTime(2024, 9, 12); 
+            Console.WriteLine($"Чи є {testDate.ToString("d")} Днем програміста? " + (isProgrammersDay(testDate) ? "Так" : "Ні"));
 
-            DateChecked += (sender, e) => Console.WriteLine("Дата була перевірена.");
-
-            DateTime testDate = new DateTime(2024, 9, 13); 
-            bool result = isProgrammerDay(testDate);
-            OnDateChecked(); 
-
-            Console.WriteLine(result ? "Це День програміста!" : "Це не День програміста.");
-        }
-
-        protected static void OnDateChecked()
-        {
-            DateChecked?.Invoke(null, EventArgs.Empty);
+            testDate = new DateTime(2024, 9, 13); 
+            Console.WriteLine($"Чи є {testDate.ToString("d")} Днем програміста? " + (isProgrammersDay(testDate) ? "Так" : "Ні"));
         }
     }
 }
